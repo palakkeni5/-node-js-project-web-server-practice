8ee196eb3eef3eeb05732a53401df48b9bba1e24
palakkeni5 <palakkeni@gmail.com>
2018-08-23 22:30:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express= require('express');
+const hbs= require('hbs');
+const fs = require('fs');
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials')
+
+app.set('view engine' , 'hbs');
+
+
+
+app.use( (req, res, next)=>{
+    
+    var now= new Date().toString();
+    var log=`${now}: ${req.method} ${req.url} `;
+    fs.appendFileSync('server.log', log+'\n');
+    console.log(log);
+    next();
+} )
+
+// app.use((req, res, next)=>{
+
+//     res.render('maintenance.hbs',{
+//         pageTitle: 'Server under Maintenance',
+//         paraText: 'We will be back soon'
+//     })
+// })
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req,res)=>{
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        paraText: 'Welcome to the home page'
+    })
+})
+app.get('/about',(req, res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+    });
+})
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        errorMessage:'Unable to connect'
+    })
+})
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
